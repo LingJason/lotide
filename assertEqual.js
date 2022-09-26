89641d2b7a3fd71306486f858740a00e6215b05b
Jason Ling <Jasonling1996@gmail.com>
2022-09-26 19:47:08 +0000

    Updated to use require

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log('👎👎👎 Assertion Failed: ' + actual + ' !== ' + expected);
-  } else if (actual === expected) {
-    console.log('👍👍👍 Assertion Passed: ' + actual + ' === ' + expected);
-  }
+    console.log(`👎👎👎 Assertion Failed:${actual} !== ${expected}`);
+    return;
+  } 
+    console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
 };
 
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 10);
+module.exports = assertEqual;
