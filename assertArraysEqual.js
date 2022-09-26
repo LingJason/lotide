fbc3e07a7fe92eef6bd9915ae88440dd536bab55
Jason Ling <Jasonling1996@gmail.com>
2022-09-26 20:32:06 +0000

    Updated to use require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,11 @@
- const eqArrays = function (array1, array2) {
-  if(array1.length === array2.length){
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-  } else {
-    return false;
-  }
-}
-
-
- const assertArraysEqual = function (array1, array2) {
-  if(!eqArrays(array1, array2)){
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = function (array1, array2) {
+  if (!eqArrays(array1, array2)) {
     return 'Not same';
   }
-  else{
+  else {
     return 'Same Same';
   }
-}
+};
 
-console.log(assertArraysEqual([1, 2, 3, 5], [1, 2, 3]));
-
-
+module.exports = assertArraysEqual;
