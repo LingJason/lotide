6c27fbd1f58a88bd268e819b53f05e35be6ea379
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:45:38 +0000

    Updated with require

--- without.js
+++ without.js
@@ -1,39 +1,23 @@
-const eqArrays = function (array1, array2) {
-  if(array1.length === array2.length){
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-  } else {
-    return false;
-  }
-}
+// const eqArrays = require('./eqArrays');
 
 
- const assertArraysEqual = function (array1, array2) {
-  if(!eqArrays(array1, array2)){
-    return 'Not same';
-  }
-  else{
-    return 'Same Same';
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 
-const without = function(array1, array2){
+const withOut = function(array1, array2) {
   let newArray = [];
-  for(const item of array1){
-    if(!array2.includes(item)){
-      newArray.push(item)
+  for (const item of array1) {
+    if (!array2.includes(item)) {
+      newArray.push(item);
     }
   }
   return newArray;
-}
- console.log(without([1, 2, 3], [1])); // => [2, 3]
- console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+};
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"]));
 console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+
+module.exports = withOut;
