fbab34f202d24579443b7a11e519b2f704807743
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:39:40 +0000

    Updated with require

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@ const assertEqual = function(actual, expected) {
   if (actual !== expected) {
     console.log(`👎👎👎 Assertion Failed:${actual} !== ${expected}`);
     return;
-  } 
-    console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
+  }
+  console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
 };
 
 module.exports = assertEqual;
