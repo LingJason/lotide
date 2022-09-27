30e876f68d3b53249db674cb976292621e08ee3c
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:45:05 +0000

    Updated with require

--- tail.js
+++ tail.js
@@ -2,7 +2,7 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-module.export = tail;
+module.exports = tail;
 // Test Case: Check the original array
 //const words = ["Yo Yo", "Lighthouse", "Labs"];
 //tail(words); // no need to capture the return value since we are not checking it
