64f50eb5fdedfbf95fac8bea5aaccb75b4985f88
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:43:28 +0000

    Updated with require

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,21 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`👎👎👎 Assertion Failed:${actual} !== ${expected}`);
-  } else if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed:${actual} === ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
 const findKeyByValue = function(genre, show) {
   return Object.keys(genre).find(key => genre[key] === show);
-}
-
+};
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = findKeyByValue;
