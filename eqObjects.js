6bc2d4175820866a6158e72ce4d10c3d50fbfddd
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:42:50 +0000

    Updated with require

--- eqObjects.js
+++ eqObjects.js
@@ -1,15 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if(array1.length === array2.length){
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-  } else {
-    return false;
-  }
-}
+const eqArrays = require("./eqArrays");
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
@@ -21,11 +10,11 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of key1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if(!eqArrays(object1[key], object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
       }
     } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
-      if(!eqObjects(object1[key], object2[key])) {
+      if (!eqObjects(object1[key], object2[key])) {
         return false;
       }
     } else {
@@ -37,19 +26,21 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
+//const ab = { a: "1", b: "2" };
+//const ba = { b: "2", a: "1" };
 // console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
- // console.log(eqObjects(ab, abc)); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
 
-const ba2 = { a: "2", b: "2"};
+// const ba2 = { a: "2", b: "2"};
 // console.log(eqObjects(ba2, ab));
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// console.log(eqObjects(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
+
+module.exports = eqObjects;
