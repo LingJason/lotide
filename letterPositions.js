6be88150a9f3c966aaf981b373d0ca836c806fd5
Jason Ling <Jasonling1996@gmail.com>
2022-09-27 00:43:58 +0000

    Updated with require

--- letterPositions.js
+++ letterPositions.js
@@ -1,41 +1,25 @@
-const eqArrays = function (array1, array2) {
-  if(array1.length === array2.length){
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-  } else {
-    return false;
-  }
-}
+// const eqArrays = require('./eqArrays');
 
 
- const assertArraysEqual = function (array1, array2) {
-  if(!eqArrays(array1, array2)){
-    return 'Not same';
-  }
-  else{
-    return 'Same Same';
-  }
-}
+// const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
     let char = sentence[i];
-    if(char === ' ') {
-      continue
+    if (char === ' ') {
+      continue;
     }
     
     if (!results[char]) {
       results[char] = [];
     }
 
-    results[char].push(i)
+    results[char].push(i);
   }
   return results;
 };
 
 console.log(letterPositions("hello"));
+
+module.exports = letterPositions;
